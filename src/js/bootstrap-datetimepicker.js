109cc0e70572a441ed02345319c365764cdc14f5
Mayfield, Philip (PCL) <pmayfield@hagroup.com>
2017-02-27 11:41:39 -0800

    fixed icon buttons

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -238,11 +238,11 @@
 
                 if (isEnabled('h')) {
                     topRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementHour }).addClass('btn btn-primary').attr('data-action', 'incrementHours').append(getSvgIcon(options.icons.up, 'white'))));
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementHour }).addClass('btn btn-primary btn-icon').attr('data-action', 'incrementHours').append(getSvgIcon(options.icons.up, 'white'))));
                     middleRow.append($('<td>')
                         .append($('<span>').addClass('timepicker-hour').attr({ 'data-time-component': 'hours', 'title': options.tooltips.pickHour }).attr('data-action', 'showHours')));
                     bottomRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementHour }).addClass('btn btn-primary').attr('data-action', 'decrementHours').append(getSvgIcon(options.icons.down, 'white'))));
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementHour }).addClass('btn btn-primary btn-icon').attr('data-action', 'decrementHours').append(getSvgIcon(options.icons.down, 'white'))));
                 }
                 if (isEnabled('m')) {
                     if (isEnabled('h')) {
@@ -251,12 +251,12 @@
                         bottomRow.append($('<td>').addClass('separator'));
                     }
                     topRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementMinute }).addClass('btn btn-primary').attr('data-action', 'incrementMinutes')
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementMinute }).addClass('btn btn-primary btn-icon').attr('data-action', 'incrementMinutes')
                             .append(getSvgIcon(options.icons.up, 'white'))));
                     middleRow.append($('<td>')
                         .append($('<span>').addClass('timepicker-minute').attr({ 'data-time-component': 'minutes', 'title': options.tooltips.pickMinute }).attr('data-action', 'showMinutes')));
                     bottomRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementMinute }).addClass('btn btn-primary').attr('data-action', 'decrementMinutes')
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementMinute }).addClass('btn btn-primary btn-icon').attr('data-action', 'decrementMinutes')
                             .append(getSvgIcon(options.icons.down, 'white'))));
                 }
                 if (isEnabled('s')) {
@@ -266,12 +266,12 @@
                         bottomRow.append($('<td>').addClass('separator'));
                     }
                     topRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementSecond }).addClass('btn btn-primary').attr('data-action', 'incrementSeconds')
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.incrementSecond }).addClass('btn btn-primary btn-icon').attr('data-action', 'incrementSeconds')
                             .append(getSvgIcon(options.icons.up, 'white'))));
                     middleRow.append($('<td>')
                         .append($('<span>').addClass('timepicker-second').attr({ 'data-time-component': 'seconds', 'title': options.tooltips.pickSecond }).attr('data-action', 'showSeconds')));
                     bottomRow.append($('<td>')
-                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementSecond }).addClass('btn btn-primary').attr('data-action', 'decrementSeconds')
+                        .append($('<a>').attr({ href: '#', tabindex: '-1', 'title': options.tooltips.decrementSecond }).addClass('btn btn-primary btn-icon').attr('data-action', 'decrementSeconds')
                             .append(getSvgIcon(options.icons.down, 'white'))));
                 }
 
